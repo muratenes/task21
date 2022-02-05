f945f64f3a8a9c9f3423dbc3551536de96511bbc
muratenes <m9urat@gmail.com>
2022-02-05 23:22:55 +0300

    created TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskStoreRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TaskStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $task = Task::create(array_merge($data, [
+            'user_id' => $request->user()->id,
+            'status' => Task::TODO
+        ]));
+
+        return response(['data' => $task], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Task $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Task $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Task $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
