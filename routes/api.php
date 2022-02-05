c071102763e5cf326e2913afa1fcc77ad5268892
muratenes <m9urat@gmail.com>
2022-02-05 23:23:28 +0300

    added User auth controller

--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+// AuthRoutes
+Route::post('/register', [\App\Http\Controllers\Auth\UserAuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Auth\UserAuthController::class, 'login']);
+
+
+Route::apiResource('/tasks', \App\Http\Controllers\TaskController::class)
+    ->middleware(['auth:api']);
