c9144cbbd66c956f5348ce04044be4f542a6baa2
muratenes <m9urat@gmail.com>
2022-02-05 22:19:39 +0300

    created Task model

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    // STATUS
+    const DOING = 1;
+    const TODO = 2;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'user_id'
+    ];
+}
