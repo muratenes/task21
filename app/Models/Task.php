fa313698f18161ec793dd4133e1095f3c0f88fe4
muratenes <m9urat@gmail.com>
2022-02-05 22:45:32 +0300

    added assignedUser relation to Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,6 +12,7 @@ class Task extends Model
     // STATUS
     const DOING = 1;
     const TODO = 2;
+    const DONE = 3;
 
     /**
      * The attributes that are mass assignable.
@@ -24,4 +25,24 @@ class Task extends Model
         'status',
         'user_id'
     ];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function assignedUser(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+
+    /**
+     *  sort tasks by status
+     *
+     * @return mixed
+     */
+    public static function sortByStatus()
+    {
+        return self::orderBy('title')->get()->sortBy('status');
+    }
 }
